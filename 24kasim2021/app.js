c5120b103f470cdd9ce99b3dd5e7097c9c94eca3
kamilsmr <kamils6035@gmail.com>
2021-11-24 00:25:29 +0300

    operatörler

---
+++ 24kasim2021/app.js
@@ -0,0 +1,36 @@
+// karşılaştırma operatörleri
+
+//   ==
+//   ===
+//   !=
+//   !==
+//   >
+//   <
+//   >=
+//   <=
+
+// console.log(2 == 2);
+// console.log("js" == "java");
+// console.log(2 == "2");
+// console.log(2 === "2");
+// console.log(4>2);
+// console.log(2>4);
+
+// console.log(2 != 2);
+// console.log(2 != 4);
+
+// console.log(4>=2);
+
+// Mantıksal Bağlaçlar
+
+// Not operatörü
+
+// console.log(!(2=2));
+// console.log(!(!2=2));
+
+// and operatörü
+// console.log((2 ==2) && ("ahmet" == "ahmet"));
+
+// or operatörü
+
+// console.log((4 == 2) || ("ahmet" != "ahmet"));
