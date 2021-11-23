41df64f0cd38be30cd368afee4baed0db138cba2
kamilsmr <kamils6035@gmail.com>
2021-11-24 00:55:15 +0300

    if else yapısı

--- 24kasim2021/app.js
+++ 24kasim2021/app.js
@@ -34,3 +34,43 @@
 // or operatörü
 
 // console.log((4 == 2) || ("ahmet" != "ahmet"));
+
+// const error = true;
+
+// if (error == true) {
+//   console.log("hata oluştu");
+// }
+
+// if (error == false) {
+//   console.log("hata oluştu");
+// }
+// console.log("deneme");
+
+// const user = "ali";
+
+// if (user === "ali") {
+//   console.log("kullanıcı bulundu");
+// } else {
+//   console.log("kullanıcı bulunmadı...");
+// }
+
+// const user = "mehmet";
+
+// if (user === "ali") {
+//   console.log("kullanıcı bulundu");
+// } else {
+//   console.log("kullanıcı bulunmadı...");
+// }
+
+// const process = "4";
+// if (process === 1) {
+//   console.log("işlem 1");
+// } else if (process === "2") {
+//   console.log("işlem 2");
+// } else if (process === "3") {
+//   console.log("işlem 3");
+// } else if (process === "4") {
+//   console.log("işlem 4");
+// } else {
+//   console.log("geçersiz işlem");
+// }
