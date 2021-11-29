0322c4ba7428c059322d0c9a57fd94e993cd643d
kamilsmr <kamils6035@gmail.com>
2021-11-30 00:16:46 +0300

    do while

--- 30kasim2021/app.js
+++ 30kasim2021/app.js
@@ -1,1 +1,54 @@
 // while döngüsü
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i += 1; //  i++;
+// }
+
+// let i = 10;
+
+// while (i > 0) {
+//   console.log(i);
+//   i--; // i--
+
+//   // i-=2;
+// }
+
+// break and continue
+
+// break döngüyü kırar
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(i);
+//   if (i == 5) {
+//     break;
+//   }
+//   i++;
+// }
+
+// let i = 0;
+// while (i < 10) {
+//   // sonsuz döngü oluştu
+//   if (i == 3 || i == 5) {
+//     continue;
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// çözmek için
+// let i = 0;
+// while (i < 10) {
+//   // sonsuz döngü oluştu
+//   if (i == 3 || i == 5) {
+//     i++;
+//     continue;
+//   }
+//   console.log(i);
+//   i++;
+// }
+
+// do while en az birkere çalışma garantisi var
