97c3ccfafbf667de222247dde0fcc3ab40f0bef2
kamilsmr <kamils6035@gmail.com>
2021-12-12 18:13:02 +0300

    add

--- 12Aralik2021/app.js
+++ 12Aralik2021/app.js
@@ -84,3 +84,51 @@
 // });
 
 // fakat arraye çevirirsek forEach kullanabiliriz.
+
+// const collections = Array.from(document.all);
+
+// collections.forEach(function (collection) {
+//   console.log(collection);
+// });
+
+// value = document.all[8];
+// value = document.body;
+// value = document.head;
+// value = document.location;
+// value = document.location.hostname;
+// value = document.location.port;
+// value = document.URL;
+// value = document.characterSet;
+// console.log(value);
+
+// let value;
+
+// value = document;
+
+// // Scripts
+// value = document.scripts;
+// value = document.scripts.length;
+
+// Linkler
+
+// value = document.links;
+// value = document.links[0];
+// value = document.links[document.links.length - 1];
+// value = document.links[document.links.length - 1].getAttribute("class");
+// value = document.links[document.links.length - 1].getAttribute("href");
+// value = document.links[document.links.length - 1].className;
+// value = document.links[document.links.length - 1].classList;
+
+//Forms
+
+// value = document.forms;
+// value = document.forms.length;
+// value = document.forms["form"];
+// value = document.forms[0].id;
+// value = document.forms[0].getAttribute("id");
+// value = document.forms[0].name;
+// value = document.forms[0].getAttribute("name");
+
+// value = document.forms[0].method;
+
+// console.log(value);
