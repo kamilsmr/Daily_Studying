b257aa46b7401f6fcd74cc234c8ea4c8fd6cbe55
kamilsmr <kamils6035@gmail.com>
2021-12-12 17:15:50 +0300

    Dom

---
+++ 12Aralik2021/app.js
@@ -0,0 +1,59 @@
+//  Scope kavramı
+
+// Global Scope
+
+// function a(){
+//     // Function Scope
+// }
+
+// if(){
+//     // Block Scope
+// }
+
+// Global Scope
+
+// var value1 = 10;
+// let value2 = 20;
+// const value3 = 30;
+
+// function Func() {
+//   var value1 = 40;
+//   let value2 = 50;
+//   const value3 = 60;
+//   console.log(value1, value2, value3);
+// }
+// Func();
+
+// const ta bir daha atama yapamıyoruz
+
+// console.log(value1, value2, value3);
+
+// burada şunu gördük function scope sadece içerde dışarıyı değiştirmez.
+
+// if (true) {
+//   var a = 10;
+//   let b = 20;
+//   const c = 30;
+//     console.log(a, b, c);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// burada var ile let ve const arasındaki farkı anlamış olduk. var if bloku dışında çalıştı fakat let ve const çalışmadı hata verdi.
+
+// var value1 = 10;
+// let value2 = 20;
+// const value3 = 30;
+
+// if (true) {
+//   var value1 = 40;
+//   let value2 = 50;
+//   const value3 = 60;
+//   console.log(value1, value2, value3);
+// }
+
+// console.log(value1, value2, value3);
+
+// DOM
